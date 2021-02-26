f7650f7d14322fa5b81d30f014d72cea117c3c79
azawlocki <artur.zawlocki@golem.network>
2021-02-26 16:58:33 +0100

    Disable color output using ANSI codes on Windows

--- examples/utils.py
+++ examples/utils.py
@@ -1,17 +1,35 @@
 """Utilities for yapapi example scripts."""
 import argparse
-import asyncio
+import os
+import platform
 import sys
 
-TEXT_COLOR_RED = "\033[31;1m"
-TEXT_COLOR_GREEN = "\033[32;1m"
-TEXT_COLOR_YELLOW = "\033[33;1m"
-TEXT_COLOR_BLUE = "\033[34;1m"
-TEXT_COLOR_MAGENTA = "\033[35;1m"
-TEXT_COLOR_CYAN = "\033[36;1m"
-TEXT_COLOR_WHITE = "\033[37;1m"
 
-TEXT_COLOR_DEFAULT = "\033[0m"
+is_a_tty = hasattr(sys.stdout, "isatty") and sys.stdout.isatty()
+has_ansi_support = os.environ.get("TERM") == "ANSI"
+is_windows = platform.system() == "Windows"
+
+ANSI_RED = "\033[31;1m"
+ANSI_GREEN = "\033[32;1m"
+ANSI_YELLOW = "\033[33;1m"
+ANSI_BLUE = "\033[34;1m"
+ANSI_MAGENTA = "\033[35;1m"
+ANSI_CYAN = "\033[36;1m"
+ANSI_WHITE = "\033[37;1m"
+ANSI_DEFAULT = "\033[0m"
+
+if has_ansi_support or (is_a_tty and not is_windows):
+    TEXT_COLOR_RED = ANSI_RED
+    TEXT_COLOR_GREEN = ANSI_GREEN
+    TEXT_COLOR_YELLOW = ANSI_YELLOW
+    TEXT_COLOR_BLUE = ANSI_BLUE
+    TEXT_COLOR_MAGENTA = ANSI_MAGENTA
+    TEXT_COLOR_CYAN = ANSI_CYAN
+    TEXT_COLOR_WHITE = ANSI_WHITE
+    TEXT_COLOR_DEFAULT = ANSI_DEFAULT
+else:
+    TEXT_COLOR_RED = TEXT_COLOR_GREEN = TEXT_COLOR_YELLOW = TEXT_COLOR_BLUE = \
+    TEXT_COLOR_MAGENTA = TEXT_COLOR_CYAN = TEXT_COLOR_WHITE = TEXT_COLOR_DEFAULT = ""
 
 
 def build_parser(description: str):
